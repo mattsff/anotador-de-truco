78bd74ecb07d7351b9efcaeb4e8f725dab91c391
Matias Sieff <matias.sieff@gmail.com>
2025-05-29 14:41:08 -0300

    feat: add wake lock

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <script lang="ts">
 import MainLayout from '@/layouts/MainLayout.vue'
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, onBeforeUnmount } from 'vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     MainLayout,
+  },
+  setup() {
+    let wakeLock: any = null
+
+    const requestWakeLock = async () => {
+      try {
+        if ('wakeLock' in navigator) {
+          wakeLock = await (navigator as any).wakeLock.request('screen')
+        }
+      } catch (err) {
+        // Wake Lock not supported
+      }
+    }
+
+    onMounted(() => {
+      requestWakeLock()
+      document.addEventListener('visibilitychange', requestWakeLock)
+    })
+
+    onBeforeUnmount(() => {
+      document.removeEventListener('visibilitychange', requestWakeLock)
+      if (wakeLock && wakeLock.release) {
+        wakeLock.release()
+      }
+    })
   },
 })
 </script>
